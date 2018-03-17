944107519441884a47ea0d22fd87de6af50335ef
beezu <w0rd22766@gmail.com>
2018-03-17 13:53:25 -0400

    removed curl due to fix in vimrc

--- neovim/vim_to_nvim.sh
+++ neovim/vim_to_nvim.sh
@@ -4,4 +4,3 @@ mkdir -p ~/.config/nvim/
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 sudo ln -s ~/.vimrc /root/.vimrc
 sudo ln -s ~/.vim /root/.vim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
