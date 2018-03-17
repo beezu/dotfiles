5b1a01a22ae940d718273fbbea4f9bae82ef40dc
bee <bee@inspi.bee.creativename>
2018-03-17 17:13:14 -0400

    tmux theming

--- neovim/vim_to_nvim.sh
+++ neovim/vim_to_nvim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p ~/.config/nvim/
+sudo mkdir -p /root/.config/nvim/
 ln -s ~/.vimrc ~/.config/nvim/init.vim
+sudo ln -s ~/.vimrc /root/.config/nvim/init.vim
 sudo ln -s ~/.vimrc /root/.vimrc
 sudo ln -s ~/.vim /root/.vim
