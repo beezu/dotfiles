fda3d0e448be43b3e717f9d5b9bbb4e1a2e0cd7a
beezu <w0rd22766@gmail.com>
2022-04-25 14:59:26 -0400

    fix alacritty build

--- mac/build-alacritty.sh
+++ mac/build-alacritty.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd ~/
 git clone https://github.com/alacritty/alacritty && cd alacritty
+brew tap homebrew/cask-fonts
+brew install --cask font-hasklug-nerd-font
+rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-darwin
 cargo check --target=aarch64-apple-darwin
 make dmg-universal
